47fc156b569882d3267ee275c2b5398971856709
rafluc <37151139+rafluc@users.noreply.github.com>
2018-04-18 14:10:36 +0200

    Update Main.java

--- OOPL5/src/main/java/pl/edu/ur/oopl5/Main.java
+++ OOPL5/src/main/java/pl/edu/ur/oopl5/Main.java
@@ -4,6 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
         // TODO zad 3, 5, 6
+       
+    //Zadanie 3
+//       Stos stos = new Stos();
+//       for (int i = 0; i < 50; i++) {
+//           stos.Push((int) Math.random());
+//       }
+//       for (int i = 0; i < 50; i++) {
+//           System.out.println(i + 1 + "." + stos.Pop());
+//       }
+        
+	//Zadanie 5
+//       Pracownik p1 = new Pracownik("adam","adamski","ksiegowy");
+//       p1.pokazDane();
+//       Firma f = new Firma();
+//       f.dodaj();
+//       f.dodaj();
+//       f.pokazwszystkie(); 
+        
+	//Zadanie 6
+//       Liczba a = new Liczba("8");
+//       a.konwersja();
+//       a.wypisywanie();
+//       System.out.println(a.mnozenie(10));
+//       System.out.println(a.metsilnia());        
     }
     
 }
