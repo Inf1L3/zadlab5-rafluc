0c5f51b6c5fd873e9cb02600a25029d1ca8613c6
rafluc <37151139+rafluc@users.noreply.github.com>
2018-04-18 14:11:23 +0200

    Create Stos.java

---
+++ OOPL5/src/main/java/pl/edu/ur/oopl5/Stos.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.ur.oopl5;
+
+public class Stos {
+
+    private int[] stosTab = new int[50];
+    private int i;
+
+    public Stos() {
+        for (int i = 0; i < this.stosTab.length; i++) {
+            this.stosTab[i] = 0;
+        }
+        this.i = 0;
+
+    }
+
+    public double Pop() {
+        if (this.i >= 1) {
+            this.i--;
+            return this.stosTab[this.i + 1];
+        } else {
+            return this.stosTab[this.i];
+        }
+    }
+
+    public void Push(int nr) {
+        if (this.i < this.stosTab.length - 1) {
+            this.stosTab[this.i] = nr;
+            this.stosTab[this.i] = nr;
+            this.i++;
+        } else if (this.i == this.stosTab.length - 1) {
+            this.stosTab[this.i] = nr;
+        } else {
+            System.err.println("Stos jest pełny");
+        }
+    }
+}
